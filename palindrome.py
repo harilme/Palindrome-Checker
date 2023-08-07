856d9b16b8c7e668271f7de12c82c8854e799ded
harilme <70759292+harilme@users.noreply.github.com>
2023-08-07 14:34:01 +0100

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,14 @@
+def is_palindrome(s):
+    s = s.lower()  # Convert the string to lowercase for case-insensitive comparison
+    s = s.replace(" ", "")  # Remove spaces from the string
+
+    return s == s[::-1]  # Compare the string with its reverse
+
+# Get input from the user
+input_string = input("Enter a string: ")
+
+# Check if the input string is a palindrome
+result = is_palindrome(input_string)
+
+# Print the result
+print("Is the input string a palindrome?", result)
